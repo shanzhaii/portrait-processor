91142d023e3db635c5e916ca7882f864c6b5eefb
Alexandre Riou <alexandre.riou@epfl.ch>
2023-09-06 02:10:34 +0200

    remove unused function and detect number faces

--- face_detector.py
+++ face_detector.py
@@ -15,6 +15,8 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     annotated_image = np.copy(rgb_image)
 
     # Loop through the detected faces to visualize.
+    if len(face_landmarks_list) != 1:
+        print("error, not single face")
     for idx in range(len(face_landmarks_list)):
         face_landmarks = face_landmarks_list[idx]
 
@@ -55,29 +57,6 @@ def resize_and_show(image):
     else:
         img = cv2.resize(image, (math.floor(w / (h / DESIRED_HEIGHT)), DESIRED_HEIGHT))
     cv2.imshow('image', img)
-
-
-def plot_face_blendshapes_bar_graph(face_blendshapes):
-    # Extract the face blendshapes category names and scores.
-    face_blendshapes_names = [face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
-    face_blendshapes_scores = [face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
-    # The blendshapes are ordered in decreasing score value.
-    face_blendshapes_ranks = range(len(face_blendshapes_names))
-
-    fig, ax = plt.subplots(figsize=(12, 12))
-    bar = ax.barh(face_blendshapes_ranks, face_blendshapes_scores, label=[str(x) for x in face_blendshapes_ranks])
-    ax.set_yticks(face_blendshapes_ranks, face_blendshapes_names)
-    ax.invert_yaxis()
-
-    # Label each bar with values
-    for score, patch in zip(face_blendshapes_scores, bar.patches):
-        plt.text(patch.get_x() + patch.get_width(), patch.get_y(), f"{score:.4f}", va="top")
-
-    ax.set_xlabel('Score')
-    ax.set_title("Face Blendshapes")
-    plt.tight_layout()
-    plt.show()
-
 
 
 if __name__ == "__main__":
